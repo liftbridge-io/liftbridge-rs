c27d8f99aa742c1bd60eed518b94211f03520ad3
Roman Useinov <roman.useinov@gmail.com>
2020-07-17 08:33:17 +0200

    todo

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,7 @@ mod client {
         timeout: Option<Duration>,
     }
 
+    //TODO: Make a builder for this
     struct StreamOptions {
         pub group: String,
         pub replication_factor: i32,
